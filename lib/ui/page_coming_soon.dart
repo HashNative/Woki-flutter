f18bac328da62f751a10f4c50a32c996f5be85c0
Ilham Safeek <ilhamsafeek@yahoo.com>
2019-10-10 11:13:15 +0530

    first commit

---
+++ lib/ui/page_coming_soon.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_collections/utils/utils.dart';
+
+import 'package:flutter_ui_collections/services/webservices.dart';
+import 'package:flutter_ui_collections/services/apilistener.dart';
+
+class PageComingSoon extends StatefulWidget {
+  
+  @override
+  _PageComingSoonState createState() => _PageComingSoonState();
+}
+
+class _PageComingSoonState extends State<PageComingSoon> {
+  Screen size;
+ApiListener mApiListener;
+  @override
+  void initState() {
+    super.initState();
+    
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    size = Screen(MediaQuery.of(context).size);
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Ticket",
+              style:
+              TextStyle(fontFamily: "Exo2", color: backgroundColor)),
+          backgroundColor: colorCurve,
+        ),
+        body: Center(
+            child: Container(
+                // width: size.getWidthPx(300),
+                // height: size.getWidthPx(300),
+                // child: Image.asset("assets/icons/icn_coming_soon.png"),
+                child: RaisedButton(
+                      child: Text("Get Data"),
+                      onPressed: WebServices(this.mApiListener).getData,
+            )  
+                ),
+            
+                )
+            
+    );
+  }
+
+}
---
+++ lib/ui/page_home.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_collections/widgets/bottom_navigationBar.dart';
+
+import '../main.dart';
+import 'page_coming_soon.dart';
+import 'page_login.dart';
+import 'page_profile.dart';
+import 'page_search.dart';
+import 'page_settings.dart';
+import 'page_signup.dart';
+import 'page_food.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  int currentTab = 0;
+  PageController pageController;
+
+  _changeCurrentTab(int tab) {
+    //Changing tabs from BottomNavigationBar
+    setState(() {
+      currentTab = tab;
+      pageController.jumpToPage(0);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pageController = new PageController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          drawer: Drawer(
+           child: Column(
+             children: <Widget>[
+               UserAccountsDrawerHeader(
+                 accountName: Text("Ilham"), 
+                 accountEmail: null,
+                 currentAccountPicture: CircleAvatar(
+                   child: Text("i"),
+                 )
+                 ,
+               ),
+             ],
+           ),
+          ),
+          body: bodyView(currentTab),
+          bottomNavigationBar: BottomNavBar(changeCurrentTab: _changeCurrentTab)),
+          
+    );
+  }
+
+  bodyView(currentTab) {
+    List<Widget> tabView = [];
+    //Current Tabs in Home Screen...
+    switch (currentTab) {
+      case 0:
+        //Hotels Page
+        tabView = [SearchPage()];
+        break;
+      case 1:
+        //Food Page
+        tabView = [FoodPage()];
+        break;
+      case 2:
+        //Profile Page
+        tabView = [PageComingSoon()];
+        break;
+      case 3:
+        //Setting Page
+        tabView = [SettingPage()];
+        break;
+    }
+    return PageView(controller: pageController, children: tabView);
+  }
+}
